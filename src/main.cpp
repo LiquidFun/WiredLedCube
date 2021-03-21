e78f529a27b57ac94f30b58a6dd5dfaa2295a5d1
JuliusR <>
2021-03-21 09:53:22 +0100

    switch to zero-based indices (x, y, z)

--- src/main.cpp
+++ src/main.cpp
@@ -13,11 +13,11 @@ void setup()
 
     T27::CubePlexer::setup();
 
-    for (int x = -1; x <= 1; ++x)
+    for (int x = 0; x < T27::CubePlexer::N; ++x)
     {
-        for (int y = -1; y <= 1; ++y)
+        for (int y = 0; y < T27::CubePlexer::N; ++y)
         {
-            for (int z = -1; z <= 1; ++z)
+            for (int z = 0; z < T27::CubePlexer::N; ++z)
             {
                 // bool led_on = (random(2) == 1);
                 // bool led_on = (x + y + z + 100) % 2 == 0;
